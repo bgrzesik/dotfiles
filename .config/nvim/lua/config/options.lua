6f6dd58da25a75f0eea43d71f4d974630bf24436
Bartłomiej Grzesik <bgrzesik@google.com>
2024-07-19 13:58:23 +0200

    Fix tabs

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -3,5 +3,8 @@ vim.opt.ttyfast = true
 vim.opt.hidden = true
 vim.opt.undofile = true
 
+vim.opt.shiftwidth = 4
+vim.opt.tabstop = 4
+vim.opt.expandtab = true
 vim.opt.list = true
-vim.opt.listchars = "trail:█"
+vim.opt.listchars = "tab:  ,trail:█"
