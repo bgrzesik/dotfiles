bf76eb35f977271b0cb095c71786aea56a53afbc
Bartłomiej Grzesik <3136224+bgrzesik@users.noreply.github.com>
2024-08-12 21:50:40 +0200

    Disable tabstop for C

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -1,6 +1,10 @@
+
+-- Use kernel style tabs
 vim.api.nvim_create_autocmd({"BufEnter", "BufWinEnter"}, {
     pattern = {"*.c", "*.h"},
     callback = function()
         vim.opt.tabstop = 8
+        vim.opt.shiftwidth = 8
+        vim.opt.expandtab = false
     end,
 })
