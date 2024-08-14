de4db2371bdda34779ee94163ba923cc6009166a
Bartłomiej Grzesik <3136224+bgrzesik@users.noreply.github.com>
2024-08-14 14:06:20 +0200

    Make kernel style affect just local terminal

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -1,10 +1,10 @@
 
 -- Use kernel style tabs
 vim.api.nvim_create_autocmd({"BufEnter", "BufWinEnter"}, {
-    pattern = {"*.c", "*.h"},
+    pattern = {"*.c"},
     callback = function()
-        vim.opt.tabstop = 8
-        vim.opt.shiftwidth = 8
-        vim.opt.expandtab = false
+        vim.opt_local.tabstop = 8
+        vim.opt_local.shiftwidth = 8
+        vim.opt_local.expandtab = false
     end,
 })
