351a4413270e5642310971c351aeee8594b82779
Bartłomiej Grzesik <3136224+bgrzesik@users.noreply.github.com>
2024-08-16 13:31:30 +0200

    Improve rust-analyzer settings search

--- .config/nvim/lua/plugins/rustacean.lua
+++ .config/nvim/lua/plugins/rustacean.lua
@@ -1,50 +1,75 @@
+local default_settings = {
+    ["rust-analyzer"] = {
+        checkOnSave = {
+            command = "clippy",
+        },
+        imports = {
+            granularity = {
+                group = "item",
+                enforce = true,
+            },
+            prefix = "crate",
+        },
+        cargo = {
+            allFeatures = true,
+            buildScripts = {
+                enable = true,
+            },
+        },
+        procMacro = {
+            enable = true,
+            ignored = {},
+        },
+    }
+};
+
+function combine_settings(project_root)
+    local ra = require("rustaceanvim.config.server")
+    local cwd = vim.fn.getcwd()
+
+    local vscode = ra.load_rust_analyzer_settings(project_root, {
+        settings_file_pattern = ".vscode/settings.json"
+    })
+
+    local vscode_cwd = ra.load_rust_analyzer_settings(cwd, {
+        settings_file_pattern = ".vscode/settings.json"
+    })
+
+    local rust_analyzer = ra.load_rust_analyzer_settings(project_root, {
+        settings_file_pattern = "rust_analyzer.json"
+    })
+
+    local rust_analyzer_cwd = ra.load_rust_analyzer_settings(cwd, {
+        settings_file_pattern = "rust_analyzer.json"
+    })
+
+    local combinded = vim.tbl_deep_extend(
+        "keep",
+        {},
+        default_settings,
+        vscode_cwd or {},
+        rust_analyzer_cwd or {},
+        vscode or {},
+        rust_analyzer or {}
+    )
+
+    return combinded
+end
+
 return {
     {
         "mrcjkb/rustaceanvim",
+        lazy = false,
         ft = { "rust" },
-        opts = {
-            server = {
-                default_settings = {
-                    ["rust-analyzer"] = {
-                        checkOnSave = {
-                            command = "clippy",
-                        },
-                        imports = {
-                            granularity = {
-                                group = "item",
-                                enforce = true,
-                            },
-                            prefix = "crate",
-                        },
-                        cargo = {
-                            buildScripts = {
-                                enable = true,
-                            },
-                        },
-                        procMacro = {
-                            enable = true,
-                            ignored = {},
-                        },
-                    },
+        init = function()
+            vim.g.rustaceanvim = {
+                tools = {
+                    enable_clippy = true,
                 },
-
-                settings = function(project_root)
-                    local ra = require("rustaceanvim.config.server")
-
-                    local vscode = ra.load_rust_analyzer_settings(project_root, {
-                        settings_file_pattern = ".vscode/settings.json"
-                    })
-
-                    local rust_analyzer = ra.load_rust_analyzer_settings(project_root, {
-                        settings_file_pattern = "rust_analyzer.json"
-                    })
-
-                    return vim.tbl_deep_extend("keep", {}, vscode, rust_analyzer)
-                end,
-            },
-        },
-        config = function (_, opts)
-            vim.g.rustaceanvim = opts or {}
+                server = {
+                    settings = combine_settings,
+                },
+            }
         end,
         keys = function() end,
     }
