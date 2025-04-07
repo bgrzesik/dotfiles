9c6c83d651aef08f8da5bef60693492126640ba3
Bartłomiej Grzesik <3136224+bgrzesik@users.noreply.github.com>
2025-04-07 16:49:44 +0200

    nvim: Use same formatting for header files

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -1,7 +1,7 @@
 
 -- Use kernel style tabs
 vim.api.nvim_create_autocmd({"BufEnter", "BufWinEnter"}, {
-    pattern = {"*.c"},
+    pattern = {"*.c", "*.h"},
     callback = function()
         vim.opt_local.tabstop = 8
         vim.opt_local.shiftwidth = 8
