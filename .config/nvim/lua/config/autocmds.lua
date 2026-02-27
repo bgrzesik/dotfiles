933cca3ef4e6ec504c7c741a0df14c15c3fe7473
Bartłomiej Grzesik <3136224+bgrzesik@users.noreply.github.com>
2026-02-27 17:20:02 +0100

    Add autocmd for DTS

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -19,6 +19,15 @@ vim.api.nvim_create_autocmd({"FileType"}, {
 })
 
 vim.api.nvim_create_autocmd({"FileType"}, {
+    pattern = "dts",
+    callback = function()
+        vim.opt_local.tabstop = 8
+        vim.opt_local.shiftwidth = 8
+        vim.opt_local.expandtab = false
+    end
+})
+
+vim.api.nvim_create_autocmd({"FileType"}, {
     pattern = "gitcommit",
     callback = function()
         vim.opt_local.spell = true
